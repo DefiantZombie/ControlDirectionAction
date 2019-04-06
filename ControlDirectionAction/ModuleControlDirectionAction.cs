34b824bb159fc7525ff1eac660000dbc7db41030
DefiantZombie <adobrinin@gmail.com>
2019-04-06 01:40:08 -0700

    Empty project
    * Includes MM and untested config setup

---
+++ ControlDirectionAction/ModuleControlDirectionAction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ControlDirectionAction
+{
+    public class ModuleControlDirectionAction : PartModule
+    {
+    }
+}
