5b05c60efa37f26c2a28b88e934d236b62dc5948
DefiantZombie <adobrinin@gmail.com>
2019-04-06 01:56:47 -0700

    Working module

--- ControlDirectionAction/ModuleControlDirectionAction.cs
+++ ControlDirectionAction/ModuleControlDirectionAction.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ControlDirectionAction
+﻿namespace ControlDirectionAction
 {
     public class ModuleControlDirectionAction : PartModule
     {
+        protected ModuleControlSurface _surfaceModule;
+        protected bool _ready = false;
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            _surfaceModule = part.Modules.GetModule<ModuleControlSurface>();
+            if (_surfaceModule == null)
+            {
+                part.RemoveModule(this);
+                Destroy(this);
+            }
+            else
+            {
+                _ready = true;
+            }
+        }
+
+        [KSPAction("#SSC_ControlDirectionAction_000001",
+            advancedTweakable = true, requireFullControl = false)]
+        public void ToggleControl(KSPActionParam param)
+        {
+            if (!_ready) return;
+
+            _surfaceModule.deployInvert = !_surfaceModule.deployInvert;
+        }
     }
 }
